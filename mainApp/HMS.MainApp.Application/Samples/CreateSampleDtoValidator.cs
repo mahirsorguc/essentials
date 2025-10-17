5d7ce922af16376607fb6559710c68f9f8efc243
Hasan Mahir Sorguc <ironicman@hotmail.com>
2025-10-17 14:57:48 +0200

    registered MainAppApplicationModule for fluent validation

--- mainApp/HMS.MainApp.Application/Samples/CreateSampleDtoValidator.cs
+++ mainApp/HMS.MainApp.Application/Samples/CreateSampleDtoValidator.cs
@@ -1,6 +1,27 @@
-﻿namespace HMS.MainApp.Samples;
+﻿using FluentValidation;
 
-public class CreateSampleDtoValidator
+namespace HMS.MainApp.Samples;
+
+public class CreateSampleDtoValidator : AbstractValidator<CreateSampleDto>
 {
-    
+    public CreateSampleDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MinimumLength(3)
+            .WithMessage("Name must be at least 3 characters long.")
+            .MaximumLength(200)
+            .WithMessage("Name cannot exceed 200 characters.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .WithMessage("Description is required.")
+            .MaximumLength(1000)
+            .WithMessage("Description cannot exceed 1000 characters.");
+
+        RuleFor(x => x.IsActive)
+            .NotNull()
+            .WithMessage("IsActive must be specified.");
+    }
 }
